b0959fd58f3fadeb993df700f110f47c5aece820
Daniel Rohca <danielrochadesousa@gmail.com>
2020-02-27 19:50:39 -0300

    Criar arquivo de criptografia M5

---
+++ MarvelMVVM/MarvelMVVM/Commons/Encrypt/M5.swift
@@ -0,0 +1,36 @@
+//
+//  M5.swift
+//  MarvelMVVM
+//
+//  Created by Daniel Rocha on 27/02/20.
+//  Copyright © 2020 Daniel Rocha. All rights reserved.
+//
+
+import Foundation
+import var CommonCrypto.CC_MD5_DIGEST_LENGTH
+import func CommonCrypto.CC_MD5
+import typealias CommonCrypto.CC_LONG
+
+class M5 {
+    
+    static func md5(string: String) -> String {
+        let length = Int(CC_MD5_DIGEST_LENGTH)
+        let messageData = string.data(using:.utf8)!
+        var digestData = Data(count: length)
+
+        _ = digestData.withUnsafeMutableBytes { digestBytes -> UInt8 in
+            messageData.withUnsafeBytes { messageBytes -> UInt8 in
+                if let messageBytesBaseAddress = messageBytes.baseAddress, let digestBytesBlindMemory = digestBytes.bindMemory(to: UInt8.self).baseAddress {
+                    let messageLength = CC_LONG(messageData.count)
+                    CC_MD5(messageBytesBaseAddress, messageLength, digestBytesBlindMemory)
+                }
+                return 0
+            }
+        }
+        
+        let string = digestData.map { String(format: "%02hhx", $0) }.joined()
+        
+        return string
+    }
+    
+}
