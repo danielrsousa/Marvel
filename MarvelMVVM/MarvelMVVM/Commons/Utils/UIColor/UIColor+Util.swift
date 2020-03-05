8cd81174d0af6bd2c42040a6b947fd53ab7b2b4e
Daniel Rohca <danielrochadesousa@gmail.com>
2020-03-05 19:23:38 -0300

    Adicionar botão para tirar filtro de busca

---
+++ MarvelMVVM/MarvelMVVM/Commons/Utils/UIColor/UIColor+Util.swift
@@ -0,0 +1,9 @@
+//
+//  UIColor+Util.swift
+//  MarvelMVVM
+//
+//  Created by Daniel Rocha on 05/03/20.
+//  Copyright © 2020 Daniel Rocha. All rights reserved.
+//
+
+import Foundation
---
+++ MarvelMVVM/MarvelMVVM/Modules/Home/TableCells/Header/HomeHeaderCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeHeaderCell.swift
+//  MarvelMVVM
+//
+//  Created by Daniel Rocha on 05/03/20.
+//  Copyright © 2020 Daniel Rocha. All rights reserved.
+//
+
+import UIKit
+
+class HomeHeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
